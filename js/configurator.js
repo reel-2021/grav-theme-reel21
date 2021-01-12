ff1239fcac08a5d2eb9c5afe145c759c3dacb661
Jo Pletsch <jo.pletsch@gmail.com>
2021-01-12 19:35:20 +0100

    Make child-config collapse

--- js/configurator.js
+++ js/configurator.js
@@ -1,5 +1,5 @@
 const options = document.getElementsByTagName("input");
-const child_configs = document.getElementsByClassName("child-config");
+const child_configs = document.getElementsByClassName("config-child");
 const total_element = document.getElementById("total");
 const list_element = document.getElementById("total-list");
 
@@ -9,24 +9,28 @@ function parseConfigs() {
     list_element.innerHTML = "";
 
     for (let o of options) {
-        if (o.checked && !o.parentElement.parentElement.parentElement.classList.contains("d-none")) {
-            if (o.getAttribute("data-child")) {
-                active_children.push(o.getAttribute("data-child"));
-            }
+        if (o.checked && o.hasAttribute("data-child")) {
+            active_children.push(o.getAttribute("data-child"));
         }
     }
 
     for (let e of child_configs) {
         if (active_children.includes(e.id)) {
-            e.classList.remove("d-none");
+            let bsCollapse = new bootstrap.Collapse(e, {toggle: false});
+            bsCollapse.show();
         }
         else {
-            e.classList.add("d-none");
+            let bsCollapse = new bootstrap.Collapse(e, {toggle: false});
+            bsCollapse.hide();
         }
     }
     
     for (let o of options) {
-        if (o.checked && !o.parentElement.parentElement.parentElement.classList.contains("d-none")) {
+        if (o.checked && active_children.includes(o.name)) {
+            total += parseInt(o.value);
+            list_element.innerHTML += o.parentElement.getElementsByTagName("ul")[0].outerHTML;
+        }
+        else if (o.checked && !document.getElementById(o.name).classList.contains("config-child")) {
             total += parseInt(o.value);
             list_element.innerHTML += o.parentElement.getElementsByTagName("ul")[0].innerHTML;
         }
